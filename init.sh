b5f443b71a0eba2a553a4e8d7f4251dfb5660c8d
Andrey Zaytsev <zaytsevand@outlook.com>
2021-08-06 17:45:57 +0300

    necessary little things

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# updating installation
+sudo apt-get update && sudo apt-get upgrade -y
+# adding zsh
+sudo apt install zsh -y
+# adding oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# addning fzf
+sudo apt-get install fzf -y
+# addning zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# making a backup for zsh config
+cp ~/.zshrc ~/.zshrc.bak
+# cloning .zshrc from gist
+curl https://gist.githubusercontent.com/zaytsevand/cc3d87b3d3dfbbbad343fd0b11ab5ad4/raw/010ae9b86529ee28c82e749e1e74ba3689cd24a3/.zshrc > ~/.zshrc
