0bd14e5ceb66e8a9359c8966d4a4beb1cd412cf4
Andrey Zaytsev <zaytsevand@outlook.com>
2022-01-29 12:50:59 +0300

    Adding basic ufw configuration

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install zsh git curl fzf -y
 
 # adding oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no & sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # addning zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -17,3 +17,6 @@ cp ~/.zshrc ~/.zshrc.bak
 
 # cloning .zshrc from gist
 curl https://gist.githubusercontent.com/zaytsevand/cc3d87b3d3dfbbbad343fd0b11ab5ad4/raw/010ae9b86529ee28c82e749e1e74ba3689cd24a3/.zshrc > ~/.zshrc
+
+# starting zsh in current shell
+sh -c "zsh"
